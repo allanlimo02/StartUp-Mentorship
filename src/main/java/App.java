f818ec73e495a2a368d89414166e83d7b4e4b3d6
ClintonOwino <clinton.omondi@student.moringaschool.com>
2021-09-29 09:23:54 +0300

    added root route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,21 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+
 public class App {
+    public static void main(String[] args) {
+
+        staticFileLocation("/public");
+
+        get("/",((request, response) -> {
+            Map<String,Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }), new HandlebarsTemplateEngine());
+    }
 }
